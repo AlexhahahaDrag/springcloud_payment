31c4169c8a224b6db83a322fe3a917b18daa7ea2
alexhahahaDrag <734663446@qq.com>
2020-08-14 07:37:50 +0800

    添加gateway filter

---
+++ cloud_gateway9500/src/main/java/com/alex/springcloud/config/GatewayFilter.java
@@ -0,0 +1,30 @@
+package com.alex.springcloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        String passwrod = exchange.getRequest().getQueryParams().getFirst("password");
+        if (uname == null || passwrod == null) {
+            log.info("=============== uname为 null ================");
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
