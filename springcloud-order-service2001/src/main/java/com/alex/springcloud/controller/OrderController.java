ffec2770015ed5404eb6d9b509986d159b1bdd91
majf <734663446@qq.com>
2020-10-10 18:07:08 +0800

    seata (not finish)

--- springcloud-order-service2001/src/main/java/com/alex/springcloud/controller/OrderController.java
+++ springcloud-order-service2001/src/main/java/com/alex/springcloud/controller/OrderController.java
@@ -5,7 +5,6 @@ import com.alex.springcloud.domain.Order;
 import com.alex.springcloud.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
