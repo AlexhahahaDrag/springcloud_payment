e8f24be1ab7718401347b7777746a53fa3cb55f4
majf <734663446@qq.com>
2020-10-23 15:53:58 +0800

    dockerfile 整合springboot

---
+++ docker_test/src/main/java/com/alex/springcloud/DockerMain8080.java
@@ -0,0 +1,12 @@
+package com.alex.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMain8080 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerMain8080.class, args);
+    }
+}
